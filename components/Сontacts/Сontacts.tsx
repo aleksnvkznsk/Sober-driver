074d90219db3a62cf3ad3733a1cf179e3bdd65bc
aleksnvkznsk <aleksandrov.developer@gmail.com>
2023-09-08 13:33:53 +0700

    change contacts

--- components/Сontacts/Сontacts.tsx
+++ components/Сontacts/Сontacts.tsx
@@ -5,11 +5,19 @@ export default function Сontacts() {
     <div className={style.contacts}>
       <h1 className={style.contacts__header}>Контакты</h1>
       <div className={style.contacts__container}>
+
         <div className={style.contacts__container__info}>
-          <h1 className={style.contacts__text}>Telegram / vladimirovich0004</h1>
-          <h1 className={style.contacts__text}>Телефон / +7 999 648 00 04</h1>
+          <h1 className={style.contacts__text}>Telegram</h1>
+          <h1 className={style.contacts__href}>
+            +7 999 648 00 04 / vladimirovich0004
+          </h1>
         </div>
-        <img className={style.contacts__img} src="/Cont.jpg"></img>
+
+        <div className={style.contacts__container__info}>
+          <h1 className={style.contacts__text}>Телефон</h1>
+          <h1 className={style.contacts__href}>+7 999 648 00 04</h1>
+        </div>
+
       </div>
     </div>
   );
